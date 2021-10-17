0b3815e85088569921cf927f77a830458184950b
natto8ss <haruki.eri@gmail.com>
2021-10-18 00:53:07 +0900

    課題2

--- js/top.js
+++ js/top.js
@@ -2,8 +2,19 @@ function onClick(){
     //テキストボックスの値を取得
     var value = document.getElementById("text").value;
     var element = document.getElementById('area');
-    const first = document.getElementById('first'); 
+    var count = getRandomInt(10000);
+    var post = '<div id="post' + count + '"> <p>' + value + '</p> <button class="btn" onclick="deleteBtn(this)">削除</button> </div>';
+
     //値を設定する
-    element.insertAdjacentHTML('afterbegin', '<p>' + value + '</p>');
-    first.remove();
+    element.insertAdjacentHTML('afterbegin', post);
+
 }
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
+
+function deleteBtn(event){
+    event.parentNode.remove();
+}
+  
